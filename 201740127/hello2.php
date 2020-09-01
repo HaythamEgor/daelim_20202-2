baec4be4db6ce176c87f1b8176e4a8a3b751285d
HaythamEgor <wisedain@gmail.com>
2020-09-01 16:36:03 +0900

    0901 실습

---
+++ 201740127/hello2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>대림대학교</h1>
+
+    <?php
+        echo "201740127 은다인 입니다";
+    ?>
+
+    <?php
+        echo "안녕하세요";
+    ?>
+
+</body>
+</html>
